8f04fc569dde49ca8b4883c359aaf6d3844c8acd
Martin Lorenzo Tanchanco <tanchanco@gmail.com>
2018-09-30 18:26:55 +0000

    Refined codes and added stretch projects

---
+++ mergeArrays.js
@@ -0,0 +1,14 @@
+function merge(array1, array2) {
+
+  var merged = array1.concat(array2);
+  var sorted = merged;
+
+  return sorted.sort(function(a, b) {
+    return a - b
+  });
+
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
